3b0be4d8fd9f9c90cf9e7519efadc43694cf1a78
Shinnosuke T <65712703+sing0421@users.noreply.github.com>
2022-05-18 23:50:58 +0900

    Update Vocaloid_search.user.js

--- Vocaloid_search.user.js
+++ Vocaloid_search.user.js
@@ -1,8 +1,13 @@
 // ==UserScript==
 // @name         Vocaloid_wiki
+// @namespace    
 // @version      1.0
 // @description  Songs or artists search on Vocaloid_wiki.
 // @author       sing0421
+// @match        http*://*/*
+// @grant        none
+// @updateURL	 
+// @run-at       context-menu
 // ==/UserScript==
 
 (function() {
